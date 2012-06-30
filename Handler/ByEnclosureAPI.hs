d2420621f063276a81f99280412e34d3e463ad90
Astro <astro@spaceboyz.net>
2012-07-01 00:14:15 +0200

    Model.ByEnclosureAPI: complete information set

--- Handler/ByEnclosureAPI.hs
+++ Handler/ByEnclosureAPI.hs
@@ -23,21 +23,53 @@ getByEnclosureJson = do
         enclosureDownloads url db
   -- Drop enclosures with no associated download
   let urlDownloads' = filter ((> 0) . length . snd) urlDownloads
-  urlRender <- getUrlRender
-  return $ RepJson $ toContent $
-         object $ map (\(url, downloads@(d:_)) ->
-                        url .= object [
-                                 "info_hash" .= infoHashToHex (downloadInfoHash d),
-                                 "size" .= downloadSize d,
-                                 "seeders" .= downloadSeeders d,
-                                 "leechers" .= downloadLeechers d,
-                                 "upspeed" .= downloadUpspeed d,
-                                 "downspeed" .= downloadDownspeed d,
-                                 "downloaded" .= downloadDownloaded d,
-                                 "sources" .= (map (\d' ->
-                                                    object [
-                                                     "torrent" .= urlRender (TorrentFileR (downloadUser d') (downloadSlug d') $ TorrentName $ downloadName d')
-                                                    ]
-                                                   ) downloads)
-                                ]
-                      ) urlDownloads
+  RepJson `fmap` 
+          toContent `fmap`
+          object `fmap` 
+          mapM (\(url, downloads) -> do
+                  json <- downloadToJson downloads
+                  return $ url .= json
+               ) urlDownloads'
+
+    where downloadToJson downloads@(d:_) = do
+                       sources <- mapM sourceToJson downloads
+                       return $
+                        object [ "info_hash" .= infoHashToHex (downloadInfoHash d)
+                               , "size" .= downloadSize d
+                               , "seeders" .= downloadSeeders d
+                               , "leechers" .= downloadLeechers d
+                               , "upspeed" .= downloadUpspeed d
+                               , "downspeed" .= downloadDownspeed d
+                               , "downloaded" .= downloadDownloaded d
+                               , "sources" .= sources
+                               ]
+          sourceToJson d = do
+                       do torrentLink' <- torrentLink d
+                          permaLink' <- permaLink d
+                          return $
+                           object [ "torrent" .= torrentLink'
+                                  , "permalink" .= permaLink'
+                                  , "item.id" .= downloadItem d
+                                  , "item.title" .= downloadTitle d
+                                  , "item.published" .= iso8601 (downloadPublished d)
+                                  , "item.homepage" .= downloadHomepage d
+                                  , "item.payment" .= downloadPayment d
+                                  , "item.image" .= downloadImage d
+                                  , "feed.title" .= downloadFeedTitle d
+                                  ]
+
+torrentLink d = 
+    ($ TorrentFileR 
+           (downloadUser d) 
+           (downloadSlug d) 
+           (TorrentName $ downloadName d)
+    ) `fmap`
+    getUrlRender
+
+permaLink d =
+    ((`T.append` (downloadItem d)) .
+     (`T.append` "#") .
+     -- TODO: UserFeedR
+     ($ TorrentFileR (downloadUser d) (downloadSlug d) (TorrentName "TODO"))) `fmap`
+    getUrlRender
+    
