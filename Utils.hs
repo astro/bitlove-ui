eb4e58f6a954ad2f6157406cc38140f258a98cca
Astro <astro@spaceboyz.net>
2012-07-01 00:12:42 +0200

    Utils.iso8601: fix tz offset

--- Utils.hs
+++ Utils.hs
@@ -9,7 +9,7 @@ iso8601 :: LocalTime -> String
 iso8601 time =
     formatTime defaultTimeLocale (iso8601DateFormat $ Just "%H:%M:%S") time ++
     zone
-    where zone = case formatTime defaultTimeLocale "%Z" time of
+    where zone = case formatTime defaultTimeLocale "%z" time of
                    (sig:h1:h2:m1:m2) ->
                      sig:h1:h2:':':m1:m2
                    _ ->
