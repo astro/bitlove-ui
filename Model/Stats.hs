ee818debdb0575e704e69e93a52f511d8e16deee
Astro <astro@spaceboyz.net>
2013-03-22 00:32:24 +0100

    Model.Stats: getCounter & getGauge use align_timestamp() for performance

--- Model/Stats.hs
+++ Model/Stats.hs
@@ -24,7 +24,7 @@ instance Convertible [SqlValue] StatsValue where
   
 getCounter :: Text -> InfoHash -> LocalTime -> LocalTime -> Integer -> Query StatsValue
 getCounter kind info_hash start stop interval =
-  query "SELECT TO_TIMESTAMP(FLOOR(EXTRACT(EPOCH FROM \"time\") / ?) * ?) AS t, SUM(\"value\") FROM counters WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND \"time\">=? AND \"time\"<=? GROUP BY t ORDER BY t ASC" [toSql interval, toSql interval, toSql kind, toSql info_hash, toSql start, toSql stop]
+  query "SELECT align_timestamp(\"time\", ?) AS t, SUM(\"value\") FROM counters WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND align_timestamp(\"time\", ?)>=? AND align_timestamp(\"time\", ?)<=? GROUP BY t ORDER BY t ASC" [toSql interval, toSql kind, toSql info_hash, toSql interval, toSql start, toSql interval, toSql stop]
   
 addCounter :: IConnection conn => 
               Text -> InfoHash -> Integer -> conn -> IO ()
@@ -36,4 +36,4 @@ addCounter kind infoHash increment db = do
 
 getGauge :: Text -> InfoHash -> LocalTime -> LocalTime -> Integer -> Query StatsValue
 getGauge kind info_hash start stop interval =
-  query ("SELECT TO_TIMESTAMP(FLOOR(EXTRACT(EPOCH FROM \"time\") / ?) * ?) AS t, MAX(\"value\") FROM gauges WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND \"time\">=? AND \"time\"<=? GROUP BY t ORDER BY t ASC") [toSql interval, toSql interval, toSql kind, toSql info_hash, toSql start, toSql stop]
+  query ("SELECT align_timestamp(\"time\", ?) AS t, MAX(\"value\") FROM gauges WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND align_timestamp(\"time\", ?)>=? AND align_timestamp(\"time\", ?)<=? GROUP BY t ORDER BY t ASC") [toSql interval, toSql kind, toSql info_hash, toSql interval, toSql start, toSql interval, toSql stop]
