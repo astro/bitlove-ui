fa6cd8ab90c3508c4550c8bbeb7c5f7aaa0942a9
Astro <astro@spaceboyz.net>
2015-01-28 22:14:02 +0100

    Model.Stats: simplify align_timestamp()-using queries

--- Model/Stats.hs
+++ Model/Stats.hs
@@ -24,7 +24,7 @@ instance Convertible [SqlValue] StatsValue where
   
 getCounter :: Text -> InfoHash -> LocalTime -> LocalTime -> Integer -> Query StatsValue
 getCounter kind info_hash start stop interval =
-  query "SELECT align_timestamp(\"time\", ?) AS t, SUM(\"value\") FROM counters WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND align_timestamp(\"time\", ?)>=? AND align_timestamp(\"time\", ?)<=? GROUP BY t ORDER BY t ASC" [toSql interval, toSql kind, toSql info_hash, toSql interval, toSql start, toSql interval, toSql stop]
+  query "SELECT align_timestamp(\"time\", ?) AS t, SUM(\"value\") FROM counters WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND \"time\">=? AND \"time\"<=? GROUP BY t ORDER BY t ASC" [toSql interval, toSql kind, toSql info_hash, toSql start, toSql stop]
   
 addCounter :: IConnection conn => 
               Text -> InfoHash -> Integer -> conn -> IO ()
@@ -37,8 +37,8 @@ addCounter kind infoHash increment db = do
 getDownloadCounter :: Text -> LocalTime -> LocalTime -> Integer -> Query StatsValue
 getDownloadCounter path start stop interval =
   -- | "info_hash LIKE 'GET /%.torrent'" to use VIEW counters_get
-  query "SELECT align_timestamp(\"time\", ?) AS t, SUM(\"value\") FROM counters WHERE info_hash LIKE 'GET /%.torrent' AND info_hash='GET '||?::BYTEA AND align_timestamp(\"time\", ?)>=? AND align_timestamp(\"time\", ?)<=? GROUP BY t ORDER BY t ASC" [toSql interval, toSql path, toSql interval, toSql start, toSql interval, toSql stop]
+  query "SELECT align_timestamp(\"time\", ?) AS t, SUM(\"value\") FROM counters WHERE info_hash LIKE 'GET /%.torrent' AND info_hash='GET '||?::BYTEA \"time\">=? AND \"time\"<=? GROUP BY t ORDER BY t ASC" [toSql interval, toSql path, toSql start, toSql stop]
 
 getGauge :: Text -> InfoHash -> LocalTime -> LocalTime -> Integer -> Query StatsValue
 getGauge kind info_hash start stop interval =
-  query ("SELECT align_timestamp(\"time\", ?) AS t, MAX(\"value\") FROM gauges WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND align_timestamp(\"time\", ?)>=? AND align_timestamp(\"time\", ?)<=? GROUP BY t ORDER BY t ASC") [toSql interval, toSql kind, toSql info_hash, toSql interval, toSql start, toSql interval, toSql stop]
+  query ("SELECT align_timestamp(\"time\", ?) AS t, MAX(\"value\") FROM gauges WHERE \"kind\"=? AND \"info_hash\"=?::BYTEA AND \"time\">=? AND \"time\"<=? GROUP BY t ORDER BY t ASC") [toSql interval, toSql kind, toSql info_hash, toSql start, toSql stop]
