e40a89d5bbd9b07bac584fa4a581a6102889ed49
Astro <astro@spaceboyz.net>
2017-11-17 01:16:37 +0100

    Handler.WebSeeder: add Range support, forward headers

--- Handler/WebSeeder.hs
+++ Handler/WebSeeder.hs
@@ -5,21 +5,22 @@ import Control.Monad.Trans
 import Control.Monad.Trans.Resource
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
-import qualified Data.ByteString as B
+import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text as T
 import Yesod
 import Data.Conduit
-import Network.HTTP.Conduit
-import Network.HTTP.Types (statusCode)
+import Network.HTTP.Conduit hiding (proxy)
+import Network.HTTP.Types.Status
 import Network.HTTP.Types.Header
 import Data.Binary.Builder (fromByteString)
+import qualified Data.CaseInsensitive as CI
 
 import Prelude
 import Foundation
 import PathPieces
 import Model.Download (torrentEnclosures)
 
--- |TODO: Content-Length, Range, CORS
+
 getWebSeedR :: HexInfoHash -> Handler (ContentType, Content)
 getWebSeedR (HexInfoHash infoHash) = do
   urls <- withDB $ torrentEnclosures infoHash
@@ -30,17 +31,31 @@ getWebSeedR (HexInfoHash infoHash) = do
                  "\n" ++ T.unpack url
               ) urls
 
+  mRange <- lookupHeader hRange
+
   manager <- httpManager <$> getYesod
   let tryUrl :: [Text] -> Handler (ContentType, Content)
-      tryUrl (url:urls) = do
+      tryUrl (url:urls') = do
         req <- parseRequest $ T.unpack url
+        let reqHeaders =
+              case mRange of
+                Just range -> [(hRange, range)]
+                Nothing -> []
         res <- liftResourceT $
-               http req manager
+               http req { requestHeaders = reqHeaders } manager
+        let resStatus = responseStatus res
         case res of
-          _ | statusCode (responseStatus res) == 200 -> do
+          _ | resStatus `elem` [ok200, partialContent206] -> do
                 let contentType =
                       fromMaybe "application/octet-stream" $
                       hContentType `lookup` responseHeaders res
+                    forwardHeader name =
+                      maybe (return ())
+                      (addHeader (decodeUtf8 $ CI.original name) . decodeUtf8) $
+                      name `lookup` responseHeaders res
+                forwardHeader hContentLength
+                forwardHeader hContentRange
+                addHeader "Access-Control-Allow-Origin" corsOrigins
 
                 (src, srcFinalizer) <-
                   liftResourceT $
@@ -56,11 +71,16 @@ getWebSeedR (HexInfoHash infoHash) = do
                           yield $ Chunk $
                             fromByteString buf
                           proxy
-                return (contentType, ContentSource src')
-          _ ->
+                sendResponseStatus resStatus (contentType, ContentSource src')
+          _ -> do
+            liftIO $ putStrLn $
+              "Webseed failed with HTTP status " ++ show resStatus
             -- Try next URL
-            tryUrl urls
+            tryUrl urls'
       tryUrl [] =
         notFound
 
   tryUrl urls
+
+  where
+    corsOrigins = "http://bitlove.org http://www.bitlove.org https://bitlove.org https://www.bitlove.org"
